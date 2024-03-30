a3153404bf66a70a71859f3ef24ae344a420642f
Samuel <samuel.batistela12@gmail.com>
2024-03-29 21:01:00 -0300

    feat: add rotas de produtos

--- index.php
+++ index.php
@@ -3,20 +3,39 @@ $metodo = $_SERVER['REQUEST_METHOD'];
 $partesRota = explode("/", $_SERVER['REQUEST_URI']);
 if ($partesRota[1] == "clientes") {
   if ($metodo == "POST") {
-    require_once "controlers/Clientes/Cliente_cadastrar.php";
+    require_once "/controlers/Clientes/Cliente_cadastrar.php";
   } elseif ($metodo == "PUT") {
-    require_once "controlers/Clientes/Cliente_atualizar.php";
+    require_once "/controlers/Clientes/Cliente_atualizar.php";
   } elseif ($metodo == "DELETE") {
-    require_once "controlers/Clientes/Cliente_excluir.php";
+    require_once "/controlers/Clientes/Cliente_excluir.php";
   } elseif ($metodo == "GET") {
     if (isset($partesRota[2])) {
       if (is_numeric($partesRota[2])) {
-        require_once "controlers/Clientes/Cliente_buscar.php";
+        require_once "/controlers/Clientes/Cliente_buscar.php";
       } else if ($partesRota[2] == "") {
-        require_once "controlers/Clientes/Cliente_listar.php";
+        require_once "/controlers/Clientes/Cliente_listar.php";
       }
     } else {
-      require_once "controlers/Clientes/Cliente_listar.php";
+      require_once "/controlers/Clientes/Cliente_listar.php";
+    }
+  } else {
+    header("HTTP/1.1 404 Not Found");
+  }
+  if ($metodo == "POST") {
+    require_once "/controlers/Produtos/Produtos_cadastrar.php";
+  } elseif ($metodo == "PUT") {
+    require_once "/controlers/Produtos/Produtos_atualizar.php";
+  } elseif ($metodo == "DELETE") {
+    require_once "/controlers/Produtos/Produtos_excluir.php";
+  } elseif ($metodo == "GET") {
+    if (isset($partesRota[2])) {
+      if (is_numeric($partesRota[2])) {
+        require_once "/controlers/Produtos/Produtos_buscar.php";
+      } else if ($partesRota[2] == "") {
+        require_once "/controlers/Produtos/Produtos_listar.php";
+      }
+    } else {
+      require_once "/controlers/Produtos/Produtos_listar.php";
     }
   } else {
     header("HTTP/1.1 404 Not Found");
